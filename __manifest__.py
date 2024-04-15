d7a4fa7e2f2e8e9c64423bcb36b54b585a282068
Jefferson <jgonzalezchaca@gmail.com>
2024-04-14 22:18:25 -0500

    [NEW] Instanciando el modulo

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Payment Manager',
+    'version': '1.0',
+    'description': 'Módulo de registro de proyectos',
+    'summary': 'Módulo de registro de proyectos',
+    'author': 'Digilab Soluciones S.A.C',
+    'website': '',
+    'license': 'LGPL-3',
+    'category': '',
+    'depends': [
+        
+    ],
+    'data': [
+        
+    ],
+    'demo': [
+        
+    ],
+    'auto_install': False,
+    'application': False,
+    'assets': {
+        
+    }
+}
