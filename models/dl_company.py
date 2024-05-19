815ef2bcc4079861dbd18843e294dd6d6898c6d1
Deyvids <deyvidsalvino@gmail.com>
2024-05-19 14:10:08 -0500

    [FIX] modificando y agregando campos a la compañia

--- models/dl_company.py
+++ models/dl_company.py
@@ -4,5 +4,26 @@ class DlCompany(models.Model):
     _name = "dl.company"
 
     name = fields.Char('Nombre')
+    
+    # Campos de Compañia
+    
+    street = fields.Char('Calle')
+    street2 = fields.Char('Calle 2')
+    city = fields.Char('Ciudad')
+    state_id = fields.Many2one('res.country.state', string='Estado')
+    country_id = fields.Many2one('res.country', string='País')
+    zip = fields.Char('Código Postal')
+    phone = fields.Char('Teléfono')
+    email = fields.Char('Correo Electrónico')
+    website = fields.Char('Sitio Web')
+    vat = fields.Char('Número de IVA')
+    company_registry = fields.Char('Registro de la Compañía')
+    logo = fields.Binary('Logo')
+    currency_id = fields.Many2one('res.currency', string='Moneda')
+    active = fields.Boolean('Activo', default=True)
 
     project_ids = fields.One2many('dl.project', 'company_id', string='Proyectos')
+    
+    _sql_constraints = [
+        ('name_uniq', 'unique (name)', 'El nombre de la compañía debe ser único!')
+    ]
